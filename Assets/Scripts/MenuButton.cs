c3ef457674b8be1091e02f2ada8253bc66f1048a
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2020-04-21 01:57:44 +0300

    Epic fon hover script + parannuksia

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;  
+using System.Collections;  
+using UnityEngine.EventSystems;  
+using UnityEngine.UI;
+using TMPro;
+
+ public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+ 
+     public TMPro.TextMeshProUGUI theText;
+ 
+     public void OnPointerEnter(PointerEventData eventData)
+     {
+         theText.fontStyle = FontStyles.Italic;
+     }
+ 
+     public void OnPointerExit(PointerEventData eventData)
+     {
+         theText.fontStyle = FontStyles.Normal;
+     }
+ }
